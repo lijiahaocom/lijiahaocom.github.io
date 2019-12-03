9dc057185e8b8e6be9272edaaf262f0db0fb0bd1
root <root@localhost.localdomain>
2019-12-03 05:14:34 -0500

    hee

---
+++ a.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+Date=$3
+if [ "$Date" = "" ]
+then
+    Date=$(date +"%Y-%m-%d")
+    echo $Date
+fi
+echo $Date
+cat > /root/lijiahaocom.github.io/_posts/$Date-$1.md <<EOF   		
+layout:     post
+title:      $2 
+subtitle:   $1
+date:       $Date
+author:     RainbomSea
+header-img: img/post-web.jpg
+catalog: true
+tags:
+    - Python
+    - Pipenv
+---					
+EOF
+
